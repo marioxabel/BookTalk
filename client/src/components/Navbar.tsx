bbb3a65ab3229bf062612d92695005af9582a65f
paodac <paodac.cbc@gmail.com>
2024-12-02 13:39:23 -0600

    navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
 import SignUpForm from './SignupForm';
 import LoginForm from './LoginForm';
 import Auth from '../utils/auth';
+import './navbar.css'
 
 const AppNavbar = () => {
   const [showModal, setShowModal] = useState(false);
@@ -30,62 +31,80 @@ const AppNavbar = () => {
 
   return (
     <>
-      <Navbar bg='dark' variant='dark' expand='lg'>
+      <Navbar expand="lg" className="custom-navbar">
         <Container fluid>
-          <Navbar.Brand as={Link} to='/'>
+          <Navbar.Brand as={Link} to="/" className="brand-text">
             BookTalk
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls='navbar' />
-          <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
-            <Nav className='ml-auto d-flex'>
-              <Nav.Link as={Link} to='/'>
-                Search For Books
+          <Navbar.Toggle aria-controls="navbar" />
+          <Navbar.Collapse id="navbar">
+            <Nav className="ml-auto d-flex align-items-center">
+              <Nav.Link as={Link} to="/" className="nav-item">
+                Search
+              </Nav.Link>
+              <Nav.Link as={Link} to="/myBooks" className="nav-item">
+                My Books
+              </Nav.Link>
+              <Nav.Link as={Link} to="/" className="nav-item">
+                Reviews
+              </Nav.Link>
+              <Nav.Link as={Link} to="/" className="nav-item">
+                My Profile
               </Nav.Link>
               {isLoggedIn ? (
                 <>
-                  <Nav.Link as={Link} to='/saved'>
-                    See Your Books
+                  <Nav.Link as={Link} to="/saved" className="nav-item">
+                    Saved Books
                   </Nav.Link>
-                  <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+                  <Nav.Link onClick={handleLogout} className="nav-item">
+                    Logout
+                  </Nav.Link>
                 </>
               ) : (
-                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+                <Nav.Link onClick={() => setShowModal(true)} className="nav-item">
+                  Login/Sign Up
+                </Nav.Link>
               )}
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       <Modal
-        size='lg'
+        size="lg"
         show={showModal}
         onHide={() => setShowModal(false)}
-        aria-labelledby='signup-modal'>
-        <Tab.Container defaultActiveKey='login'>
+        aria-labelledby="signup-modal"
+      >
+        <Tab.Container defaultActiveKey="login">
           <Modal.Header closeButton>
-            <Modal.Title id='signup-modal'>
-              <Nav variant='pills'>
+            <Modal.Title id="signup-modal">
+              <Nav variant="pills">
                 <Nav.Item>
-                  <Nav.Link eventKey='login'>Login</Nav.Link>
+                  <Nav.Link eventKey="login">Login</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+                  <Nav.Link eventKey="signup">Sign Up</Nav.Link>
                 </Nav.Item>
               </Nav>
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <Tab.Content>
-              <Tab.Pane eventKey='login'>
-                <LoginForm handleModalClose={() => {
-                  setShowModal(false);
-                  setIsLoggedIn(Auth.loggedIn()); // Update login status
-                }} />
+              <Tab.Pane eventKey="login">
+                <LoginForm
+                  handleModalClose={() => {
+                    setShowModal(false);
+                    setIsLoggedIn(Auth.loggedIn()); // Update login status
+                  }}
+                />
               </Tab.Pane>
-              <Tab.Pane eventKey='signup'>
-                <SignUpForm handleModalClose={() => {
-                  setShowModal(false);
-                  setIsLoggedIn(Auth.loggedIn()); // Update login status
-                }} />
+              <Tab.Pane eventKey="signup">
+                <SignUpForm
+                  handleModalClose={() => {
+                    setShowModal(false);
+                    setIsLoggedIn(Auth.loggedIn()); // Update login status
+                  }}
+                />
               </Tab.Pane>
             </Tab.Content>
           </Modal.Body>
