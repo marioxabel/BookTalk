ac3b222d6423cd6fb97c01260190c22af33fb688
paodac <paodac.cbc@gmail.com>
2024-12-05 13:35:45 -0600

    startpage css edit

--- client/src/pages/StartPage.tsx
+++ client/src/pages/StartPage.tsx
@@ -7,76 +7,76 @@ import Auth from '../utils/auth';
 import './StartPage.css'
 
 const StartPage = () => {
-    const [showLoginModal, setShowLoginModal] = useState(false);
-    const [showSignupModal, setShowSignupModal] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(Auth.loggedIn());
-    const navigate = useNavigate(); 
-  
-    useEffect(() => {
-      if (isLoggedIn) {
-        navigate("/searchbooks")
-      }
-    }, [isLoggedIn, navigate]);
-  
-    return (
-        <div className="startpage">
-          <h1>Hi there! ready to read some books?</h1>
-          
-          {/* Login Button */}
-          <button 
-            type="button" 
-            className="btn btn-outline-dark" 
-            onClick={() => setShowLoginModal(true)}
-          >
-            Login
-          </button>
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const [showSignupModal, setShowSignupModal] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(Auth.loggedIn());
+  const navigate = useNavigate();
 
-          {/* Signup Button */}
-          <button 
-            type="button" 
-            className="btn btn-outline-dark" 
-            onClick={() => setShowSignupModal(true)}
-          >
-            Sign Up
-          </button>
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/searchbooks");
+    }
+  }, [isLoggedIn, navigate]);
 
-          {/* Login Modal */}
-          <Modal
-            size='lg'
-            show={showLoginModal}
-            onHide={() => setShowLoginModal(false)}
-            aria-labelledby='login-modal'
-          >
-            <Modal.Header closeButton>
-              <Modal.Title id='login-modal'>Login</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <LoginForm handleModalClose={() => {
-                setShowLoginModal(false);
-                setIsLoggedIn(Auth.loggedIn()); // Update login status
-              }} />
-            </Modal.Body>
-          </Modal>
+  return (
+    <div className="startpage">
+      <h1>Hi there! Ready to read some books?</h1>
+      
+      {/* Login Button */}
+      <button 
+        type="button" 
+        className="btn" 
+        onClick={() => setShowLoginModal(true)}
+      >
+        Login
+      </button>
 
-          {/* Signup Modal */}
-          <Modal
-            size='lg'
-            show={showSignupModal}
-            onHide={() => setShowSignupModal(false)}
-            aria-labelledby='signup-modal'
-          >
-            <Modal.Header closeButton>
-              <Modal.Title id='signup-modal'>Sign Up</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <SignUpForm handleModalClose={() => {
-                setShowSignupModal(false);
-                setIsLoggedIn(Auth.loggedIn()); // Update login status
-              }} />
-            </Modal.Body>
-          </Modal>
-        </div>
-    );
-  };
-  
-  export default StartPage;
+      {/* Signup Button */}
+      <button 
+        type="button" 
+        className="btn" 
+        onClick={() => setShowSignupModal(true)}
+      >
+        Sign Up
+      </button>
+
+      {/* Login Modal */}
+      <Modal
+        size="lg"
+        show={showLoginModal}
+        onHide={() => setShowLoginModal(false)}
+        aria-labelledby="login-modal"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="login-modal">Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <LoginForm handleModalClose={() => {
+            setShowLoginModal(false);
+            setIsLoggedIn(Auth.loggedIn());
+          }} />
+        </Modal.Body>
+      </Modal>
+
+      {/* Signup Modal */}
+      <Modal
+        size="lg"
+        show={showSignupModal}
+        onHide={() => setShowSignupModal(false)}
+        aria-labelledby="signup-modal"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="signup-modal">Sign Up</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <SignUpForm handleModalClose={() => {
+            setShowSignupModal(false);
+            setIsLoggedIn(Auth.loggedIn());
+          }} />
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default StartPage;
